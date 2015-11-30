a71bf378d09c32354fc1d18aae6e0693181320ad
Dharmendra0630 <frndk.king@gmail.com>
2015-11-30 12:27:57 +0530

    Update ComExample.java
    i did one thing print one messagr

--- CoreJava/src/comparable/ComExample.java
+++ CoreJava/src/comparable/ComExample.java
@@ -4,6 +4,8 @@ private int rno;
 private String name;
 private String surname;
 ComExample(int rno,String name,String surname){
+	
+	System.out.println("This message show's changes");
 	this.rno=rno;
 	this.name=name;
 	this.surname=surname;
