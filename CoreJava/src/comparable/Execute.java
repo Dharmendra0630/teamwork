9a8db1cd2740f9578ccc3ee762cdff10b8722ec7
hemant <hemantjava@gmail.com>
2015-11-30 11:20:52 +0530

    name modified

--- CoreJava/src/comparable/Execute.java
+++ CoreJava/src/comparable/Execute.java
@@ -5,7 +5,7 @@ public static void main(String[] args) {
 		ComExample c[]=new ComExample[3];
 		c[0]=new ComExample(333,"vinod","tivari");
 		c[1]=new ComExample(303,"hemant","tivari");
-		c[2]=new ComExample(233,"naveen","tivari");
+		c[2]=new ComExample(233,"naveen","mishra");
 
 	Arrays.sort(c);
 
