0795ce049f330c905513d95e0942ab0079fd5a81
Dharmendra0630 <frndk.king@gmail.com>
2015-11-30 12:40:54 +0530

    Update ComExample.java
    check budy its changed

--- CoreJava/src/comparable/ComExample.java
+++ CoreJava/src/comparable/ComExample.java
@@ -4,6 +4,7 @@ private int rno;
 private String name;
 private String surname;
 ComExample(int rno,String name,String surname){
+	System.out.println("i m changed");
 	this.rno=rno;
 	this.name=name;
 	this.surname=surname;
